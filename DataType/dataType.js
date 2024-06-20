0ce40113157a335291bf2bfac07e1ffb9a3ae28d
Sagarraj2001 <sagarraj19042001@gmail.com>
2024-06-20 10:34:39 +0530

    Data Type folder created

---
+++ DataType/dataType.js
@@ -0,0 +1,9 @@
+let num="Amount=500 or profit= 50.20"
+document.getElementById("num_data").innerHTML=num;
+
+let str="Name='Sagar Raj' or place='Patna' etc."
+document.getElementById("str_data").innerHTML=str;
+
+let bool="true and false.";
+document.getElementById("bool_data").innerHTML=bool;
+
