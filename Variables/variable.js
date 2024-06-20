600bc983361b246956427f045db11c1ffd5e00b6
Sagarraj2001 <sagarraj19042001@gmail.com>
2024-06-20 10:16:16 +0530

    Variable folder is ready

---
+++ Variables/variable.js
@@ -0,0 +1,7 @@
+//Variable Definition
+let var_intr="A Variable is a container that contains some values.<br> Ex:-"
+document.getElementById("var_intro").innerHTML=var_intr;
+
+//Examples
+let ex= "Name='Sagar Raj'<br> Profit=2000 etc."
+document.getElementById("example").innerHTML=ex;
