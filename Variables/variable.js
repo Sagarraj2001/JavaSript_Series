0ab1cd930c452a83781f756f3178abd892d22509
Sagarraj2001 <sagarraj19042001@gmail.com>
2024-06-20 10:23:37 +0530

    Variable folder completed

--- Variables/variable.js
+++ Variables/variable.js
@@ -5,3 +5,7 @@ document.getElementById("var_intro").innerHTML=var_intr;
 //Examples
 let ex= "Name='Sagar Raj'<br> Profit=2000 etc."
 document.getElementById("example").innerHTML=ex;
+
+//explanation
+let exp="here,<br> Name and Profit are Variable name which is also says as identification or label.<br> 'Sagar Raj' and 2000 are the actual value which are conatined by these label.<br> '=' is assignment operator to define the value."
+document.getElementById("explanation").innerHTML=exp;
